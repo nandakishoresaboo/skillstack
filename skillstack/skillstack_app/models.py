bf4e5b6613760a09618a4d2c4409b7be694c5602
avinashprasad <k.avinash.prasad@gmail.com>
2014-03-28 22:16:43 +0530

    Added subs for User model

--- skillstack/skillstack_app/models.py
+++ skillstack/skillstack_app/models.py
@@ -1,3 +1,21 @@
-from django.db import models
-
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class SkillSetUser(User):
+   stack_user = models.ForeignKey(StackUser)
+   yahoo_user = models.ForeignKey(YahooUser)
+
+
+class StackUser(models.Model):
+    pass
+
+class YahooUser(models.Model):
+    pass
+
+
+
+
+
+ 
