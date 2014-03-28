b9650f5bfa0cfc2455a1ca4ade0aac947fd031d1
nandakishore <nandakishoresaboo@yahoo.com>
2014-03-28 22:45:57 +0530

    updating test_ln.py

--- skillstack/skillstack/test_ln.py
+++ skillstack/skillstack/test_ln.py
@@ -2,25 +2,22 @@ import oauth2 as oauth
 import httplib2
 import time, os, simplejson
 import pprint
+import yaml
 
-# Fill the keys and secrets you retrieved after registering your app
-consumer_key = 'k5dropmvdua8'
-consumer_secret = '28lEolfpDG2Q0C7w'
-user_token = '148ddc0c-6886-48fa-8333-0381a2ea2e82'
-user_secret = 'eb11a1e8-25b2-4810-ab48-f7d255b9574f'
+# read skillstack yaml file
+yc = yaml.load(open("./skillstack.yaml"))
+pprint.pprint(yc)
 
 # Use your API key and secret to instantiate consumer object
-consumer = oauth.Consumer(consumer_key, consumer_secret)
+consumer = oauth.Consumer(yc['linkedin']['consumer_key'], yc['linkedin']['consumer_secret'])
 
 # Use your developer token and secret to instantiate access token object
-access_token = oauth.Token(
-            key=user_token,
-            secret=user_secret)
+access_token = oauth.Token(key=yc['linkedin']['user_token'], secret=yc['linkedin']['user_secret'])
 
 client = oauth.Client(consumer, access_token)
 
 # Make call to LinkedIn to retrieve your own profile
-resp,content = client.request("http://api.linkedin.com/v1/people/~:(skills)", "GET", "")
+resp,content = client.request(yc['linkedin']['skills'], "GET", "")
 
 # By default, the LinkedIn API responses are in XML format. If you prefer JSON, simply specify the format in your call
 #resp,content = client.request("http://api.linkedin.com/v1/people/~?format=json", "GET", "")
